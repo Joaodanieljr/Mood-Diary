4287dd5ccbefcd891f46c137bd272a4a2e208bc7
joao daniel <joaodanieljr@gmail.com>
2019-12-19 21:43:54 -0300

    Created Esqueci senha + new style + new shape

--- app/src/main/java/com/joaodanieljr/mooddiary/Activities/LoginActivity.kt
+++ app/src/main/java/com/joaodanieljr/mooddiary/Activities/LoginActivity.kt
@@ -14,22 +14,30 @@ class LoginActivity : AppCompatActivity() {
 
         val buttonLogin: Button
         val textCadastro: TextView
+        val textEsqueceu: TextView
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         buttonLogin = findViewById(R.id.buttonLogin)
         textCadastro = findViewById(R.id.textCadastro)
+        textEsqueceu = findViewById(R.id.textEsqueceuSenha)
 
         buttonLogin.setOnClickListener{
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
 
+
         textCadastro.setOnClickListener {
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
 
+        }
+
+        textEsqueceu.setOnClickListener{
+            val intent = Intent(this,EsqueceuSenhaActivity::class.java)
+            startActivity(intent)
         }
     }
 
